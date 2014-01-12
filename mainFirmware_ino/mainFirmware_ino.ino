bdbc5e39423191675bb192c4c12f14f948e3707a
dcampbel <dncswim76@gmail.com>
2014-01-12 18:51:49 -0500

    "added start of main file"

---
+++ mainFirmware_ino/mainFirmware_ino.ino
@@ -0,0 +1,23 @@
+
+/*full array for the sphere*/
+char fullBallArray[128][360];
+
+/*the deg we are on, 
+ * or the index of the full 
+ *array*/
+int currentDegNumber;
+
+/*to calculate the frequency
+ * the ball is spinning*/
+float timeA, timeB;
+float frequency;
+
+
+void setup(){};
+
+/*Interupt Service Routine
+ * -linked with Hall effect sensor*/
+void updateFrequency(){}
+
+void loop(){};
+
