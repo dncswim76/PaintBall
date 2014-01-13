3d7a093d87d5fd9f2251e1e90572a955ee3d61ea
dcampbel <dncswim76@gmail.com>
2014-01-12 21:25:40 -0500

    added display function

--- mainFirmware_ino/mainFirmware_ino.ino
+++ mainFirmware_ino/mainFirmware_ino.ino
@@ -1,9 +1,10 @@
 #include <FrequencyTimer2.h>
 
-#define INTERRUPTINT 0 /*digital pin 2 on arduinoMEGA*/
+#define INTERRUPTINT (0) /*digital pin 2 on arduinoMEGA*/
+#define PRECISION (360)
 
 /*full array for the sphere*/
-char fullBallArray[128][360];
+char fullBallArray[360][16];
 
 /*the deg we are on, 
  * or the index of the full 
@@ -15,6 +16,9 @@ int currentDegNumber;
 unsigned long timeA, timeB;
 unsigned long frequency;
 
+/*temporary pin values (to be changed later*/
+char rowPins[8] = {0, 1, 2, 3, 4, 5, 6, 7};
+char colPins[16] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
 
 void setup(){
   
@@ -29,7 +33,19 @@ void setup(){
 }
 
 /*the function that will constant refresh the array to display the column*/
-void displayFunc(){}
+void displayFunc(){
+
+  /*loop through the column*/
+  for (int i = 0; i < 8; i ++){
+      digitalWrite(rowPins[i], HIGH);
+     for (int j = 0; j < 16; j++){
+       /*not so sure on these bitwise operators .... but we'll see*/
+       digitalWrite(colPins[j], (fullBallArray[currentDegNumber][2*i+j/8]>>(j%8))&1);
+     }
+     digitalWrite(rowPins[i], LOW);
+  }
+  
+}
   
 
 
@@ -42,7 +58,8 @@ void updateFrequency(){
   /*number of milliseconds since the program started*/
   timeB = millis();
   /*calculate frequence in milliseconds*/
-  frequency = (timeB - timeA)/360;
+  frequency = (timeB - timeA)/PRECISION;
+  /*restart the display to 0*/
   currentDegNumber = 0;  
   
 }
