25905c9848d207badd9e1f224029762f2b8b02a6
dcampbel <dncswim76@gmail.com>
2014-01-12 20:54:21 -0500

    added ISR and started display function*/

--- mainFirmware_ino/mainFirmware_ino.ino
+++ mainFirmware_ino/mainFirmware_ino.ino
@@ -1,3 +1,6 @@
+#include <FrequencyTimer2.h>
+
+#define INTERRUPTINT 0 /*digital pin 2 on arduinoMEGA*/
 
 /*full array for the sphere*/
 char fullBallArray[128][360];
@@ -9,15 +12,48 @@ int currentDegNumber;
 
 /*to calculate the frequency
  * the ball is spinning*/
-float timeA, timeB;
-float frequency;
+unsigned long timeA, timeB;
+unsigned long frequency;
 
 
-void setup(){};
+void setup(){
+  
+  attachInterrupt(INTERRUPTINT, updateFrequency, RISING); 
+  timeA = 0;
+  timeB = 0;
+  
+  FrequencyTimer2::disable();
+  FrequencyTimer2::setPeriod(200);
+  FrequencyTimer2::setOnOverflow(displayFunc);
+
+}
+
+/*the function that will constant refresh the array to display the column*/
+void displayFunc(){}
+  
+
 
 /*Interupt Service Routine
  * -linked with Hall effect sensor*/
-void updateFrequency(){}
+void updateFrequency(){
+  
+  /*store old time*/
+  timeA = timeB;
+  /*number of milliseconds since the program started*/
+  timeB = millis();
+  /*calculate frequence in milliseconds*/
+  frequency = (timeB - timeA)/360;
+  currentDegNumber = 0;  
+  
+}
 
-void loop(){};
+void loop(){
+
+  /* step to the next degree*/  
+  currentDegNumber++;
+  
+  /*delay in milliseconds*/
+  delay(frequency);
+
+}
 
