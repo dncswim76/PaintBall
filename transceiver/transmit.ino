213eb8dca2f30a0cb7a49406b9e74d06ee77fccc
Brian Ip <bci@andrew.cmu.edu>
2014-01-16 20:51:47 -0500

    fixed transmit

--- transceiver/transmit.ino
+++ transceiver/transmit.ino
@@ -9,8 +9,6 @@
 #include "Nrf2401.h"
 
 Nrf2401 Radio;
-int incomingByte = 0;
-int i = 0;
 
 void setup(void){
     Serial.begin(9600);
@@ -21,11 +19,7 @@ void setup(void){
 
 void loop(){
     if (Serial.available() > 0) {
-        Radio.data[i] = Serial.read();
-        i = i + 1;
-        if (i == 18){
-            Radio.write();
-            i = 0;
-        }
+        Serial.readBytes(Radio.data, 18);
+        Radio.write();
     }
 }
